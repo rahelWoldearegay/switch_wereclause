d29fe050a996edfdc2d4a2b23cbe90accc479b05
Reta <124452409+rahelWoldearegay@users.noreply.github.com>
2023-03-25 15:24:40 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+var wholeNumber = Int.random(in: 10...20)
+print("Generated number: \(wholeNumber)")
+
+switch wholeNumber {
+case let x where x % 2 == 0:
+    print("Composite")
+case let x where x % 3 == 0:
+    print("Composite")
+default:
+    print("Prime")
+}
